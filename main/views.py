dab7dcf5cd502efacfb9eb70fae70ee5e90968d1
kolyuhahaha <kolyuhahaha@mail.ru>
2025-02-21 17:40:13 +0300

    Первый пример

--- main/views.py
+++ main/views.py
@@ -5,7 +5,10 @@ from django.http import HttpResponse
 def index(request):
     context = {
         'title': 'Главная',
-        'content': 'Главная страница магазина'
+        'content': 'Главная страница магазина',
+        'prods': {'sku': 1001, 'name': 'Мясорубка Tefal TF 4501', 'price': 4990},
+        'list': [True, 125, 'Товар'],
+        'bool': False
     }
     return render(request, 'main/index.html', context=context)
 
